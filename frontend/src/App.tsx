0df7f6185a3df4801927ac5129d4e5f3fdd55ff3
Subhankar Rajbanshi <subhankar18r@gmail.com>
2025-04-07 19:18:08 +0530

    change backend link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -28,7 +28,7 @@ function App() {
 
     try {
       const response = await axios.post<MCQResponse>(
-        "https://quiztube-backend.onrender.com/api/generate-mcq",
+        "https://044a4176-8ff1-4a81-86d7-d042818e2cb2-00-1pdvuk0iqw2vd.sisko.replit.dev/api/generate-mcq",
         {
           videoUrl,
         }
